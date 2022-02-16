4c05c3977b9e190141d32e0a95298efc27c73685
Ashutosh Kumar <bds.ashu@gmail.com>
2022-02-16 23:25:49 +0530

    Update navebar.js

--- components/navebar.js
+++ components/navebar.js
@@ -8,11 +8,11 @@ document.write(`
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../quzz/">Home</a>
+                    <a class="nav-link active" aria-current="page" href="../">Home</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="../quzz/PAGES/index2.html">Online Courses</a>
+                    <a class="nav-link " aria-current="page" href="/quzz/PAGES/index2.html">Online Courses</a>
                 </li>
 
                 <li class="nav-item">
